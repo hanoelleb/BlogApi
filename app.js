6e6e4de43c7cc0fc9d34dd027afedb09180f7ad9
hanoelleb <hanoelleb@gmail.com>
2020-07-19 16:52:03 -0700

    reconfigure cors

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true
+}));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
