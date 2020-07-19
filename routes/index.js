c79b885a2cfb4f18b169ad582ad828e85892dcd2
hanoelleb <hanoelleb@gmail.com>
2020-07-19 16:38:52 -0700

    enabled cors on routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var cors = require('cors');
+
 var authController = require('../controllers/auth');
 var postController = require('../controllers/post');
+
+router.use(cors());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
