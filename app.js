3d00cc9aa34e86b411167934f61e95c1e5aaa406
hanoelleb <hanoelleb@gmail.com>
2020-07-19 17:13:02 -0700

    trying alternative

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ var app = express();
 
 require('dotenv').config();
 
-app.options('*', cors());
+//app.options('*', cors());
+
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 //app.use(cors());
 
 var mongoose = require('mongoose');
